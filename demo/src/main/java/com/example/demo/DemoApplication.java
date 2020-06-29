65cb41e3a0525773e6b2a2a157218bd151674a1f
TomoakiTakaichi <kiruhihhohu555@gmail.com>
2020-06-29 20:24:20 +0900

    refactor: tab to space

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,13 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
-	@GetMapping
-    public String hello() {
-        return "hello world";
-    }
-
+  @GetMapping
+  public String hello() {
+    return "hello world";
+  }
 }
